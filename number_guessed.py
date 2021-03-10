1618b3d49b5a64df600246f096afd948f75a4972
Jagoda <jrutkowska7@st.swps.edu.pl>
2021-03-10 23:40:01 +0100

    guess_num

--- number_guessed.py
+++ number_guessed.py
@@ -1,22 +1,62 @@
-print("Bienvenido a nuestro juego! \n") 
+print("BIENVENIDO A NUESTRO JUEGO! \n") 
 
 import random
-rand_num = random.randint(1,101)
+rand_num = random.randint(1,11)
 score = 100
 
-print(f"You have one job: guess a number from 1 to 100. Watch out! More attempts you have, less points you get. \n \nYou have {score} points for the good start. Mucha suerte mi amigo. \n ")
+print(f"You have one job: guess a number from 1 to 10. Watch out! More attempts you have, less points you get. You have maximum four attempts.  \nYou have {score} points for the good start. \nMucha suerte mi amigo. Let's start! \n ")
 
 def first_clue(rand_num):
     if rand_num % 2 == 0:
-        return "I will help you mi amigo. The number you need to guess is even"
+        return "Okay, I will help you a little. The number you need to guess is even.\n"
     else:
-        return "I will help you mi amigo. The number you need to guess is odd"
+        return "Okay, I will help you a little. The number you need to guess is odd.\n"
+
 
 print(first_clue(rand_num))
 
-user_num = input("THE NUMBER: ")
+user_num1 = int(input("THE NUMBER: "))
+
+if user_num1 == rand_num:
+    print(f"You have a luck! Guessed with the first try. You got {score} points \n")
+elif user_num1 > rand_num:
+    print (f"Sorry darling! The number you are trying to guess is smaller. You have {score -25} points \n ")
+else:
+    print (f"Sorry daring! The number you are trying to guess is bigger. You have {score -25} points \n ")
+
+
+user_num2 = int(input("SECOND ATTEMPT: "))
+
+
+if user_num2 == rand_num:
+    print(f"You have a luck! Guessed with the second try. You got {score} points\n")
+elif user_num2 > rand_num:
+    print (f"The number you are trying to guess is smaller, baby. You have {score -50} points.\n")
+else:
+    print (f"The number you are trying to guess is bigger, baby. You have {score -50} points.\n")
+
+user_num3 = int(input("THIRD ATTEMPT: "))
+
+if user_num3 == rand_num:
+    print(f"Well done. You guessed with the thirdattempt. You got {score - 75} points\n")
+elif user_num3 > rand_num:
+    print (f"The number you are trying to guess is smaller, baby. You have {score - 75} points\n")
+else:
+    print (f"The number you are trying to guess is bigger, baby. You have {score - 75} points\n")
+
+user_num4 = int(input("FOURTH, LAST ATTEMPT: "))
+
+if user_num4 == rand_num:
+    print(f"Enhorabuena! You kept your honour and guessed the number.\n")
+elif user_num4 > rand_num:
+    print (f"The number you are trying to guess is smaller, baby. You re loosing the game.\n")
+else:
+    print (f"The number you are trying to guess is bigger, baby. You are losing the game. \n")
 
 
 
 
 
+
+
+    
